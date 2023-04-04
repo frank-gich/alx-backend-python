2432bb94a4071b29f6d60b9d7b11103a71d97aca
frank-gich <gichuhi5781@gmail.com>
2023-04-05 00:15:53 +0300

    my first push

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -3,7 +3,7 @@
 import random
 import asyncio
 
-def wait_random( max_delay: int=10) -> float:
+def wait_random( max_delay: int = 10) -> float:
     """ returns a float with a random delay"""
     
     random_val = random.uniform(0, max_delay)
