9652bb972e3941b47f00a72412f95ac2fb1a4455
frank-gich <gichuhi5781@gmail.com>
2023-04-07 10:33:01 +0300

    my first push

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 
-import random
 import asyncio
+import random
+
 
 def wait_random( max_delay: int = 10) -> float:
     """ returns a float with a random delay"""
     
     random_val = random.uniform(0, max_delay)
-    await asyncio.sleep(random_val)
+    asyncio.sleep(random_val)
     return random_val
