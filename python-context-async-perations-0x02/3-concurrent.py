08ad9e389691170441e638789a016667b1bf42d0
frank-gich <gichuhi5781@gmail.com.com>
2025-07-09 21:01:49 +0300

    Context Managers and Asynchronous programming in python

--- python-context-async-perations-0x02/3-concurrent.py
+++ python-context-async-perations-0x02/3-concurrent.py
@@ -29,7 +29,7 @@ async def async_fetch_users():
             print("All Users:")
             for user in users:
                 print(user)
-
+            #return
 async def async_fetch_older_users():
     async with aiosqlite.connect(DB_NAME) as db:
         async with db.execute("SELECT * FROM users WHERE age > 40") as cursor:
@@ -37,7 +37,7 @@ async def async_fetch_older_users():
             print("\nUsers older than 40:")
             for user in older_users:
                 print(user)
-
+            #return
 # --- Run both queries concurrently ---
 async def fetch_concurrently():
     await asyncio.gather(
