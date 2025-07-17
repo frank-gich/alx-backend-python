80ad482d40a8de8a1813dec192ad7c7b6dbec639
Collin-5 <collinsotieno380@gmail.com>
2025-07-17 13:56:25 +0300

    unitesting

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -46,7 +46,7 @@ class TestGetJson(unittest.TestCase):
         "utils.requests.get"
     )
     def test_get_json(self, test_url, test_payload, mock_get):
-        """Test get_json returns correct payload and calls requests.get once"""
+        """Test get_json returns correct payload and makes one GET request."""
         mock_response = Mock()
         mock_response.json.return_value = test_payload
         mock_get.return_value = mock_response
