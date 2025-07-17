906ccb3c5c4786d7a9fda023365c31ef834ff1fb
Collin-5 <collinsotieno380@gmail.com>
2025-07-17 23:16:57 +0300

    django app

---
+++ messaging_app/chats/serializers.py
@@ -0,0 +1,55 @@
+# chats/serializers.py
+
+from rest_framework import serializers
+from .models import CustomUser, Conversation, Message
+
+
+# -----------------------------
+# User Serializer
+# -----------------------------
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = [
+            'user_id',
+            'username',
+            'email',
+            'first_name',
+            'last_name',
+            'phone_number',
+        ]
+
+
+# -----------------------------
+# Message Serializer
+# -----------------------------
+class MessageSerializer(serializers.ModelSerializer):
+    sender = UserSerializer(read_only=True)  # nested sender info
+
+    class Meta:
+        model = Message
+        fields = [
+            'message_id',
+            'sender',
+            'conversation',
+            'message_body',
+            'sent_at',
+            'created_at',
+        ]
+
+
+# -----------------------------
+# Conversation Serializer
+# -----------------------------
+class ConversationSerializer(serializers.ModelSerializer):
+    participants = UserSerializer(many=True, read_only=True)
+    messages = MessageSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Conversation
+        fields = [
+            'conversation_id',
+            'participants',
+            'created_at',
+            'messages',  # nested messages
+        ]
